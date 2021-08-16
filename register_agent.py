364b5452093ede2c61c4bc4c44dbea80f479885e
Charlie Getzen <charlie@bolt.com>
2021-08-16 13:11:23 -0500

    wazuh cloud

--- register_agent.py
+++ register_agent.py
@@ -40,41 +40,50 @@ def create_config_file():
         open("local_internal_options.jinja2", "rb").read()
     )
 
+def create_auth_file():
+    # with open("authd.pass.jinja2") as file_:
+    #     template = Template(file_.read())
+    #     config = template.render(
+    #         agent_registration_password=agent_registration_password
+    #     )
+    wazuh_authd_file = open("/etc/authd.pass", "w")
+    wazuh_authd_file.write(f"{agent_registration_password}\n")
+    wazuh_authd_file.close()
 
-def req(method, resource, data=None):
-    login_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Basic {b64encode(auth).decode()}",
-    }
-    response = requests.get(login_url, headers=login_headers, verify=False)  # nosec
-    token = json.loads(response.content.decode())["data"]["token"]
-    requests_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}",
-    }
-    url = f"{base_url}/{resource}"
-    try:
-        requests.packages.urllib3.disable_warnings()
-
-        if method.lower() == "post":
-            r = requests.post(
-                url, headers=requests_headers, data=json.dumps(data), verify=verify
-            )
-        elif method.lower() == "put":
-            r = requests.put(url, headers=requests_headers, data=data, verify=verify)
-        elif method.lower() == "delete":
-            r = requests.delete(url, headers=requests_headers, data=data, verify=verify)
-        else:
-            r = requests.get(url, headers=requests_headers, params=data, verify=verify)
-
-        code = r.status_code
-        res_json = r.json()
-
-    except Exception as exception:
-        logger.error(f"Error: {resource}")
-        sys.exit(1)
-
-    return code, res_json
+# def req(method, resource, data=None):
+#     login_headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Basic {b64encode(auth).decode()}",
+#     }
+#     response = requests.get(login_url, headers=login_headers, verify=False)  # nosec
+#     token = json.loads(response.content.decode())["data"]["token"]
+#     requests_headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {token}",
+#     }
+#     url = f"{base_url}/{resource}"
+#     try:
+#         requests.packages.urllib3.disable_warnings()
+#
+#         if method.lower() == "post":
+#             r = requests.post(
+#                 url, headers=requests_headers, data=json.dumps(data), verify=verify
+#             )
+#         elif method.lower() == "put":
+#             r = requests.put(url, headers=requests_headers, data=data, verify=verify)
+#         elif method.lower() == "delete":
+#             r = requests.delete(url, headers=requests_headers, data=data, verify=verify)
+#         else:
+#             r = requests.get(url, headers=requests_headers, params=data, verify=verify)
+#
+#         code = r.status_code
+#         res_json = r.json()
+#
+#     except Exception as exception:
+#         logger.error(f"Error: {resource}")
+#         sys.exit(1)
+#
+#     return code, res_json
 
 
 def application_data():
@@ -110,52 +119,52 @@ def code_desc(http_status_code):
     return requests.status_codes._codes[http_status_code][0]
 
 
-def add_agent(agt_name, agt_ip=None):
-    if agt_ip:
-        status_code, response = req(
-            "post", "agents", {"name": agt_name, "ip": agt_ip, "force_time": 1}
-        )
-    else:
-        status_code, response = req(
-            "post", "agents", {"name": str(agt_name), "force_time": 1}
-        )
-
-    if status_code == 200 and response["error"] == 0:
-        r_id = response["data"]["id"]
-        r_key = response["data"]["key"]
-        return r_id, r_key
-    else:
-        msg = json.dumps(response, indent=4, sort_keys=True)
-        code = f"Status: {status_code} - {code_desc(status_code)}"
-        logger.error(f"ERROR - ADD AGENT:\n{code}\n{msg}")
-        exit(1)
-
-
-def info_agent(agt_name, pretty=None):
-    if pretty:
-        status_code, response = req("get", f"agents?pretty=true&q=name={agt_name}")
-    else:
-        status_code, response = req("get", f"agents?q=name={agt_name}")
-    if status_code == 200 and response["error"] == 0:
-        for items in response["data"]["affected_items"]:
-            name = items["name"]
-            status = items["status"]
-        return name, status
-    else:
-        msg = json.dumps(response, indent=4, sort_keys=True)
-        code = f"Status: {status_code} - {code_desc(status_code)}"
-        logger.error(f"ERROR - ADD AGENT:\n{code}\n{msg}")
-        exit(1)
+# def add_agent(agt_name, agt_ip=None):
+#     if agt_ip:
+#         status_code, response = req(
+#             "post", "agents", {"name": agt_name, "ip": agt_ip, "force_time": 1}
+#         )
+#     else:
+#         status_code, response = req(
+#             "post", "agents", {"name": str(agt_name), "force_time": 1}
+#         )
+#
+#     if status_code == 200 and response["error"] == 0:
+#         r_id = response["data"]["id"]
+#         r_key = response["data"]["key"]
+#         return r_id, r_key
+#     else:
+#         msg = json.dumps(response, indent=4, sort_keys=True)
+#         code = f"Status: {status_code} - {code_desc(status_code)}"
+#         logger.error(f"ERROR - ADD AGENT:\n{code}\n{msg}")
+#         exit(1)
 
 
-def import_key(agent_key):
-    cmd = "/var/ossec/bin/manage_agents"
-    std_out, std_err, r_code = execute([cmd, "-i", agent_key], "y\n\n")
-    if r_code != 0:
-        logger.error(f"ERROR - Import key:{std_err}")
-        exit(1)
-    else:
-        logger.info(f"INFO - Key has been imported {std_out}")
+# def info_agent(agt_name, pretty=None):
+#     if pretty:
+#         status_code, response = req("get", f"agents?pretty=true&q=name={agt_name}")
+#     else:
+#         status_code, response = req("get", f"agents?q=name={agt_name}")
+#     if status_code == 200 and response["error"] == 0:
+#         for items in response["data"]["affected_items"]:
+#             name = items["name"]
+#             status = items["status"]
+#         return name, status
+#     else:
+#         msg = json.dumps(response, indent=4, sort_keys=True)
+#         code = f"Status: {status_code} - {code_desc(status_code)}"
+#         logger.error(f"ERROR - ADD AGENT:\n{code}\n{msg}")
+#         exit(1)
+
+
+# def import_key(agent_key):
+#     cmd = "/var/ossec/bin/manage_agents"
+#     std_out, std_err, r_code = execute([cmd, "-i", agent_key], "y\n\n")
+#     if r_code != 0:
+#         logger.error(f"ERROR - Import key:{std_err}")
+#         exit(1)
+#     else:
+#         logger.info(f"INFO - Key has been imported {std_out}")
 
 
 def execute(cmd_list, stdin=None):
@@ -174,7 +183,7 @@ def execute(cmd_list, stdin=None):
 
 def restart_ossec():
     cmd = "/var/ossec/bin/ossec-control"
-    std_out, std_err, r_code = execute([cmd, "restart"])
+    std_out, std_err, r_code = execute([cmd, "start"])
     restarted = False
 
     for line_output in std_out.split(os.linesep):
@@ -204,17 +213,18 @@ def status_ossec():
 
 if __name__ == "__main__":
     try:
-        protocol = os.environ.get("JOIN_MANAGER_PROTOCOL")
-        host = os.environ.get("JOIN_MANAGER_MASTER_HOST")
+        # protocol = os.environ.get("JOIN_MANAGER_PROTOCOL")
+        # host = os.environ.get("JOIN_MANAGER_MASTER_HOST")
+        # port = os.environ.get("JOIN_MANAGER_API_PORT")
         user = os.environ.get("JOIN_MANAGER_USER")
         password = os.environ.get("JOIN_MANAGER_PASSWORD")
         node_name = os.environ.get("NODE_NAME")
-        port = os.environ.get("JOIN_MANAGER_API_PORT")
-        join_manager_port = os.environ.get("JOIN_MANAGER_PORT")
+        join_manager_port = os.environ.get("JOIN_MANAGER_PORT") # agent registration port
+        agent_registration_password = os.environ.get("AGENT_REGISTRATION_PASSWORD")
         groups = os.environ.get("WAZUH_GROUPS")
         health_checks = os.environ.get("HEALTH_CHECK_PROCESSES")
         virus_total_key = os.environ.get("VIRUS_TOTAL_KEY")
-        join_manager_worker = os.environ.get("JOIN_MANAGER_WORKER_HOST")
+        join_manager_worker = os.environ.get("JOIN_MANAGER_WORKER_HOST") # # agent registration server
         flask_bind = os.environ.get("FLASK_BIND")
         if "," not in groups:
             groups = "default,"
@@ -223,49 +233,51 @@ if __name__ == "__main__":
             group_list = list(groups.split(","))
         if not node_name:
             node_name = os.environ.get("HOSTNAME")
-        if not protocol:
-            protocol = "https"
-        if not (
-            protocol and host and user and node_name and join_manager_port,
-            groups,
-            join_manager_worker,
-        ):
-            raise KeyError
+        # if not protocol:
+        #     protocol = "https"
+        # if not (
+        #     protocol and host and user and node_name and join_manager_port,
+        #     groups,
+        #     join_manager_worker,
+        # ):
+        #     raise KeyError
 
     except KeyError as error:
         logger.error(f"Please check system variable {error}")
         exit(2)
 
     login_endpoint = "security/user/authenticate"
-    base_url = f"{protocol}://{host}:{port}"
-    login_url = f"{protocol}://{host}:{port}/{login_endpoint}"
+    # base_url = f"{protocol}://{host}:{port}"
+    # login_url = f"{protocol}://{host}:{port}/{login_endpoint}"
     auth = f"{user}:{password}".encode()
 
     verify = False
     logger.info(f"Adding agent with name {node_name}")
-    agent_id, agent_key = add_agent(node_name)
+    # agent_id, agent_key = add_agent(node_name)
+    agent_id = "charlie"
     logger.info(f"Agent '{node_name}' with ID '{agent_id}' added.")
     logger.info(f"Importing authentication key for agent {node_name}")
-    import_key(agent_key.encode())
+    # import_key(agent_key.encode())
     logger.info(f"Create OSSEC configuration for agent {node_name}")
     create_config_file()
+    create_auth_file()
     logger.info(f"Restarting. Agent {node_name}.....")
     restart_ossec()
     logger.info(f"Getting status of OSSEC processes for agent {node_name}......")
     status_ossec()
-    status = True
-    while status:
-        agent_name, agent_status = info_agent(node_name)
-        if agent_status == "active":
-            logger.info(
-                f"Agent '{agent_name}' is ready and connected,  status - '{agent_status}......"
-            )
-            logger.info(
-                f"Agent {agent_name} has been connected to server {join_manager_worker}......"
-            )
-            status = False
-        else:
-            logger.info(
-                f"Waiting for agent {agent_name} become ready current status is {agent_status}......"
-            )
+    # status = True
+    # while status:
+    #     agent_name, agent_status = info_agent(node_name)
+    #     if agent_status == "active":
+    #         logger.info(
+    #             f"Agent '{agent_name}' is ready and connected,  status - '{agent_status}......"
+    #         )
+    #         logger.info(
+    #             f"Agent {agent_name} has been connected to server {join_manager_worker}......"
+    #         )
+    #         status = False
+    #     else:
+    #         logger.info(
+    #             f"Waiting for agent {agent_name} become ready current status is {agent_status}......"
+    #         )
     app.run(host=flask_bind)
